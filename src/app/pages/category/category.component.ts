e006eace7f6db2a74f4cdc83b8c764231a2f7728
Martin Mendez <martinpercu@gmail.com>
2023-06-14 18:27:17 +0100

    callback hell solution in ngOnit

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators'
 
 import { ProductsService } from './../../services/products.service';
 import { Product } from '../../models/product.model'
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
+  // templateUrl: './category.component.html',
+  template: `<app-products [products]="products" (loadMore)="loadMoreProducts()"></app-products>`,
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
@@ -22,18 +24,33 @@ export class CategoryComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.categoryId = params.get('id');
-      if(this.categoryId) {
-        console.log(this.categoryId);
-        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-        .subscribe(data => {
-          this.products = data;
-        })
-      }
-
+    this.route.paramMap
+    .pipe(
+      switchMap(params => {
+        this.categoryId = params.get('id');
+        if(this.categoryId) {
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        }
+        return [];
+      })
+    )
+    .subscribe((data) => {
+      this.products = data
     });
   }
+
+  // ngOnInit(): void {
+  //   this.route.paramMap.subscribe(params => {
+  //     this.categoryId = params.get('id');
+  //     if(this.categoryId) {
+  //       console.log(this.categoryId);
+  //       this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+  //       .subscribe(data => {
+  //         this.products = data;
+  //       })
+  //     }
+  //   });
+  // }
 
 
   loadMoreProducts() {
