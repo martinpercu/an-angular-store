f851f99e8ca1517fd237863a9872b92d47938bf3
Martin Mendez <martinpercu@gmail.com>
2023-06-23 15:58:48 +0100

    big refactor for nested view + adding the website module to manage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,58 +1,67 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { CategoryComponent } from './pages/category/category.component';
-import { MyCartComponent } from './pages/my-cart/my-cart.component';
-import { RecoveryComponent } from './pages/recovery/recovery.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { LayoutComponent } from './website/components/layout/layout.component';
+import { HomeComponent } from './website/pages/home/home.component';
+import { RegisterComponent } from './website/pages/register/register.component';
+import { LoginComponent } from './website/pages/login/login.component';
+import { ProfileComponent } from './website/pages/profile/profile.component';
+import { NotFoundComponent } from './website/pages/not-found/not-found.component';
+import { CategoryComponent } from './website/pages/category/category.component';
+import { MyCartComponent } from './website/pages/my-cart/my-cart.component';
+import { RecoveryComponent } from './website/pages/recovery/recovery.component';
+import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
+
 
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent
-  },
-  {
-    path: 'notfound',
-    component: NotFoundComponent
-  },
-  {
-    path: 'category/:id',
-    component: CategoryComponent
-  },
-  {
-    path: 'product/:id',
-    component: ProductDetailComponent
-  },
-  {
-    path: 'mycart',
-    component: MyCartComponent
-  },
-  {
-    path: 'recovery',
-    component: RecoveryComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: 'notfound',
+        component: NotFoundComponent
+      },
+      {
+        path: 'category/:id',
+        component: CategoryComponent
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent
+      },
+      {
+        path: 'mycart',
+        component: MyCartComponent
+      },
+      {
+        path: 'recovery',
+        component: RecoveryComponent
+      }
+
+    ]
   },
   {
     path: '**',
--- src/app/website/components/products/products.component.ts
+++ src/app/website/components/products/products.component.ts
@@ -4,10 +4,10 @@ import { zip } from 'rxjs'
 import { Location } from '@angular/common'
 
 
-import { Product, CreateProductDTO, UpdateProductDTO } from '../../models/product.model'
+import { Product, CreateProductDTO, UpdateProductDTO } from '../../../models/product.model'
 
-import { StoreService } from '../../services/store.service'
-import { ProductsService } from '../../services/products.service'
+import { StoreService } from '../../../services/store.service'
+import { ProductsService } from '../../../services/products.service'
 
 
 @Component({
